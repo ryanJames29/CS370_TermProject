e8e3fd72361c8b101f19e64d6b57a0e8543fe7f9
TianaGabel <Tianamg78@gmail.com>
2024-05-08 16:46:55 -0600

    Final code

--- piNetwork/network/client_v2.py
+++ piNetwork/network/client_v2.py
@@ -1,25 +1,48 @@
 import socket
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Connecting")
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('10.0.0.148', 12345)
-client_socket.connect(server_address)
+    hostname = socket.gethostname()
+    print(hostname)
+    server_address = ('192.168.1.28', 12345)
+    client_socket.connect(server_address)
 
-welcome_message = client_socket.recv(1024).decode()
-print(welcome_message)
+    welcome_message = client_socket.recv(1024).decode()
+    print(welcome_message)
 
-initial_message = client_socket.recv(1024).decode()
-print(initial_message)
-
-while True:
+    username = client_socket.recv(1024).decode()
+    print(username)
     message = input("Desktop: ")
-    
     client_socket.send(message.encode())
-    
-    if message.lower() == 'quit':
-        break
-    
-    response = client_socket.recv(1024).decode()
-    print(response)
 
-client_socket.close()
+    password = client_socket.recv(1024).decode()
+    print(password)
+    message = input("Desktop: ")
+    client_socket.send(message.encode())
+
+    ID_prompt = client_socket.recv(1024).decode()
+    print(ID_prompt)
+    client_socket.send(message.encode())
+
+    welcome_message = client_socket.recv(1024).decode()
+    print("1",welcome_message)
+
+    welcome_message = client_socket.recv(1024).decode()
+    print("2",welcome_message)
+
+    while True:
+        message = input("Desktop: ")
+
+        client_socket.send(message.encode())
+
+        if message.lower() == 'quit':
+            break
+
+        response = client_socket.recv(1024).decode()
+        print(response)
+        response = client_socket.recv(1024).decode()
+        print(response)
+finally:
+    client_socket.close()
--- piNetwork/network/server_v2.py
+++ piNetwork/network/server_v2.py
@@ -1,23 +1,72 @@
 import socket
 import threading
+import os
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 from gpiozero import LED
-from time import sleep
+from gpiozero.pins.native import NativeFactory
+from gpiozero import Device
+
+GPIO.setmode(GPIO.BOARD)
+
+Device.pin_factory = NativeFactory()
 
 red_LED = LED(26)
 yellow_LED = LED(20)
 green_LED = LED(6)
 
 red_LED.on()
+# (Username, Password) : physical id
+users = {('Desktop1', 'CS370'): 596170611542,
+         ('Desktop2', 'CS370'): 584195496087}
 
 numClients = []
 
+
+def check_credentials(username_attempt, password_attempt):
+    if (username_attempt, password_attempt) in users:
+        return True
+    return False
+
+
+def check_id_card(usercredentials):
+    reader = SimpleMFRC522()
+    try:
+        uid = reader.read_id()
+        if (users.get(usercredentials) == uid):
+            return True
+        else:
+            return False
+    finally:
+        GPIO.cleanup()
+
+
+def set_light(size):
+    if size == 0:
+        red_LED.on()
+        green_LED.off()
+        yellow_LED.off()
+    elif size == 1:
+        red_LED.off()
+        green_LED.off()
+        yellow_LED.on()
+    else:
+        red_LED.off()
+        yellow_LED.off()
+        green_LED.on()
+
+
+def send_message(filename, client_socket):
+    base = '/home/tia/nfcproject/370Project/serverFiles/'
+    file_path = file_path = os.path.join(base, filename)
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    client_socket.send(str(file_content).encode())
+    # client_socket.send('\n'.encode())
+
+
 def multipleClients(client_socket, addr):
     print(f"Connection from {addr}\n")
-
-    username1 = 'Desktop1'
-    username2 = 'Desktop2'
-    password1 = 'CS370'
-    password2 = 'CS370'
 
     client_socket.send("Welcome to the server, ran by Raspberry Pi 5!\n".encode())
 
@@ -28,70 +77,52 @@ def multipleClients(client_socket, addr):
     client_socket.send("Password: ".encode())
     password_attempt = client_socket.recv(1024).decode().strip()
 
-    # Check username and password
-    if (username_attempt == username1 and password_attempt == password1) or \
-    (username_attempt == username2 and password_attempt == password2):
-        numClients.append(addr) 
-        size = len(numClients)
-        if size == 1:
-            red_LED.off()
-            yellow_LED.on()
-        elif size == 2:
-            red_LED.off()
-            yellow_LED.off()
-            green_LED.on()
-        client_socket.send("Login successful!\n".encode())
-        # Proceed with further actions after successful login
-        with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/list.txt', 'r') as file:
-            file_content = file.read()
-        client_socket.send(file_content.encode())
+    if check_credentials(username_attempt, password_attempt):
+        client_socket.send("Please scan id card".encode())
+        message = client_socket.recv(1024).decode()
+        if check_id_card((username_attempt, password_attempt)):
+            client_socket.send("ID approved \n".encode())
+            numClients.append(addr)
+            print(f"Successful login from {username_attempt} at {addr}\n")
+            set_light(len(numClients))
 
-        try:
-            while True:
-                #Message from the client
-                message = client_socket.recv(1024).decode()
-                print("Desktop:", message)
+            client_socket.send("Login successful!\n".encode())
+            # Proceed with further actions after successful login
+            send_message('list.txt', client_socket)
 
-                if message.lower() == 'planets':
-                    with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/planets.txt', 'r') as file:
-                        file_content = file.read()
-                    client_socket.send(file_content.encode())
-                elif message.lower() == 'pi':
-                    with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/pi.txt', 'r') as file:
-                        file_content = file.read()
-                    client_socket.send(file_content.encode())
-                elif message.lower() == 'homepage':
-                    with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/Homepage.txt', 'r') as file:
-                        file_content = file.read()
-                    client_socket.send(file_content.encode())
-                elif message.lower() == 'project':
-                    with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/Project.txt', 'r') as file:
-                        file_content = file.read()
-                    client_socket.send(file_content.encode())
-                elif message.lower() == 'list':
-                    with open('/home/ryanjames/Desktop/CS370_TermProject/piNetwork/serverFiles/list.txt', 'r') as file:
-                        file_content = file.read()
-                    client_socket.send(file_content.encode())
-                elif message.lower() == 'quit':
-                    if size == 1:
-                        yellow_LED.off()
-                        green_LED.off()
-                        red_LED.on()
+            try:
+                while True:
+                    # Message from the client
+                    message = client_socket.recv(1024).decode()
+                    print("[Desktop]:", message)
+
+                    if message.lower() == 'planets':
+                        send_message('planets.txt', client_socket)
+                    elif message.lower() == 'helloworld':
+                        send_message('helloWorld.py', client_socket)
+                    elif message.lower() == 'homepage':
+                        send_message('homepage.txt', client_socket)
+                    elif message.lower() == 'project':
+                        send_message('project.txt', client_socket)
+                    elif message.lower() == 'quit':
+                        break
                     else:
-                        red_LED.off()
-                        green_LED.off()
-                        yellow_LED.on()
-                    break
-                else:
-                    client_socket.send("Need anything else? (quit to exit)".encode())
-        except Exception as e:
-            print("Error:", e)
-        finally:
-            #close the client connection
-            client_socket.close()
+                        client_socket.send(f"we don't have {message.lower()} press enter\n".encode())
+                        send_message('list.txt', client_socket)
+
+                    client_socket.send("Need anything else? (quit to exit) \n".encode())
+
+            except Exception as e:
+                print("Error:", e)
+            finally:
+                numClients.remove(addr)
+                set_light(len(numClients))
+                client_socket.close()
+        else:
+            client_socket.send("ID rejected \n".encode())
     else:
         client_socket.send("Invalid username or password!\n".encode())
-        # Close connection or take appropriate action for failed login
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -102,9 +133,12 @@ server_socket.bind((host, port))
 server_socket.listen(5)
 print("Raspberry Pi is waiting for incoming connections...")
 
-while True:
-    #accept client connection
-    client_socket, addr = server_socket.accept()
+try:
+    while True:
+        # accept client connection
+        client_socket, addr = server_socket.accept()
 
-    client_thread = threading.Thread(target=multipleClients, args=(client_socket, addr))
-    client_thread.start()
+        client_thread = threading.Thread(target=multipleClients, args=(client_socket, addr))
+        client_thread.start()
+finally:
+    server_socket.close()
